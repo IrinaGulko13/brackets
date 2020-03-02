5653e942b870187f846a84b1478558a94b8888af
Iryna Hulko <irinagulko13@gmail.com>
2020-03-02 21:11:00 +0300

    feat:add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let result='';
+    let n = 0 ;
+    while (n < bracketsConfig.length) {
+      n = 0;
+      for (let i = 0; i < bracketsConfig.length; i++) {
+        let br = bracketsConfig[i][0] + bracketsConfig[i][1];
+        result = str.replace(br, "");
+        if (str == result) {
+          n++;
+        }
+        str = result;
+      }
+    }
+    return str.length == 0;
 }
